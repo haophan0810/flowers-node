1ff76e6ea503d0b32a06cf38d98e954dd5e73a38
haophan0810 <huuhaotk0810@gmail.com>
2019-01-29 01:51:17 +0700

    rewrite index

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,48 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
-const db = require('../models');
+const db = require("../models");
 
 module.exports.getIndex = async (req, res, next) => {
-    try {
-        const dataProduct = await db.Category.findAll({
-
-            include: [{
-                model: db.Product,
-                include: [{
-                    model: db.ProductDiscount
-                }, {
-                    model: db.ProductImage
-                }, {
-                    model: db.ProductPricing
-                }]
-            },{
-                model: db.CategoryDiscount
-            }]
-        });
-
-        res.status(200).json(dataProduct);
-
-    } catch (error) {
-        throw Error(error.message);
-    }
-}
+  try {
+    const dataProducts = await db.Category.findAll({
+      where: {
+        id: {
+            [Op.between]: [1, 3]
+        }
+      },
+      order: [
+          ['id']
+      ],
+      include: [
+        {
+          model: db.Product,          
+          include: [
+            {
+              model: db.ProductDiscount
+            },
+            {
+              model: db.ProductImage
+            },
+            {
+              model: db.ProductPricing
+            }
+          ]          
+        },
+        {
+          model: db.CategoryDiscount
+        }
+      ]
+    });
+    const [productsHot, productsNew, productsSale] = dataProducts;
+    // console.log('product hot',productsHot.Products);
+    // res.status(200).json(productsHot.Products);
+    res.render('index', {
+        productsHot: productsHot,
+        productsNew: productsNew,
+        productsSale: productsSale,
+        title: 'Flowers-shop | Home'
+    })
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
