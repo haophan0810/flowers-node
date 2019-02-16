0367a3de732ecb4c18162ea6cb4b7faefe56a9df
Huu Hao Phan <huuhaotk0810@gmail.com>
2019-02-16 23:53:23 +0700

    config connect heroku-postgre

--- models/index.js
+++ models/index.js
@@ -11,16 +11,16 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  // sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    username: "skapqcfrmvdcdm",
-    password: "51a18033264772ed5e8faa795356bc74f14eb88d75bb5cfbebc3a4637eb02f01",
-    database: "d5ror6g774l4tt",
-    host: "ec2-54-243-223-245.compute-1.amazonaws.com",
-    dialect: "postgres",
-    loggin: true,
-    port: 5432
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // sequelize = new Sequelize(process.env[config.use_env_variable], {
+  //   username: "skapqcfrmvdcdm",
+  //   password: "51a18033264772ed5e8faa795356bc74f14eb88d75bb5cfbebc3a4637eb02f01",
+  //   database: "d5ror6g774l4tt",
+  //   host: "ec2-54-243-223-245.compute-1.amazonaws.com",
+  //   dialect: "postgres",
+  //   loggin: true,
+  //   port: 5432
+  // });
   
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
